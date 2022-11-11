7902200799f1fae6812d0085d4940f658fb2e655
Tony Fok <s1155110873@gmail.com>
2022-11-12 01:40:09 +0800

    added

--- Q1/solution.js
+++ Q1/solution.js
@@ -1,20 +1,3 @@
-// function translate(size){
-//     switch(size){
-//         case S:
-//             size = 1
-//             break
-//         case M:
-//             size = 2
-//             break
-//         case L:
-//             size =3
-//         default:
-//             console.log("Invalid input")
-//     }
-
-//     return size
-// }
-
 function compare(total, request){
     
     newTotal = total.slice(-1)[0]
@@ -39,15 +22,15 @@ function compare(total, request){
         requestscore += request.length
     }
 
-    console.log(`requestscore = ${requestscore}`)
-    console.log(`totalscore = ${totalscore}`)
+    // console.log(`requestscore = ${requestscore}`)
+    // console.log(`totalscore = ${totalscore}`)
 
-    if (totalscore > requestscore){
-        console.log("return true")
+    if (totalscore >= requestscore){
+        // console.log("return true")
         return true
     }
 
-    console.log("return false")
+    // console.log("return false")
     return false
     
 
@@ -56,24 +39,24 @@ function compare(total, request){
 function validate(n, total, m, request){
     
     if (n != total.length){
-        console.log('not matching total length')
+        // console.log('not matching total length')
         return
     }else if (m != request.length){
-        console.log('not matching request length')
+        // console.log('not matching request length')
         return
     }
 
     for (let y = 0; y < request.length; y++){
         for (let x = 0; x < total.length; x++){
-            console.log(total[x] + "&" + request[y])
+            // console.log(total[x] + "&" + request[y])
             let result = compare(total[x], request[y])
-            if (result === false && x === (total.length - 1))
+            if (result) break
+            
+            if (result === false && x === (total.length - 1)){
                 console.log('No')
                 return
+            } 
             }
-            
-            console.log("continue matching")
-            continue
         }
     
 
@@ -81,4 +64,4 @@ function validate(n, total, m, request){
 
 }
 
-validate(5, ["XL", "XXXXXL", "XXS", "M", "XXS"], 4, ["L", "M", "L", "XXS"])
+validate(5, ["XL", "XXL", "XXS", "M", "XS"], 4, ["XL", "M", "L", "XXXS"])
